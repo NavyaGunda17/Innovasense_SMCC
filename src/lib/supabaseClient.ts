09a95127c10b3fa07b18c3b968c2ac1b26048e47
NavyaGunda23 <navyagunda.71@gmail.com>
2025-09-08 14:49:08 +0400

    revertuing to old url

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,8 @@
 // src/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://qdwkznmbngbdnldckens.supabase.co";
+const supabaseUrl = "https://supabasesmcc.innovasense.net";
+// "https://172.64.149.246/";
 const supabaseAnonKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkd2t6bm1ibmdiZG5sZGNrZW5zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTIxMzQwNTMsImV4cCI6MjA2NzcxMDA1M30.kyS_M95tfV5VdD0tocff3u6MaULA_0T4w3GHwR-BSqw";
 
@@ -15,3 +16,16 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     },
   },
 });
+
+
+// export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+//   db: { schema: "public" },
+//   realtime: { params: { eventsPerSecond: 10 } },
+//   global: {
+//     headers: {
+//       // You can add custom headers here
+//       "Access-Control-Allow-Origin": "*",   // ⚠️ browsers usually ignore this
+//       "Access-Control-Allow-Methods": "GET, POST, OPTIONS",
+//     },
+//   },
+// });
